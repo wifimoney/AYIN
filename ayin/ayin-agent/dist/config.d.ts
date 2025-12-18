d57088edca85435490a4e4386e068e0841342417
AetherOps <elibelilty1998@gmail.com>
2025-12-18 18:03:28 +0000

    chore: stop tracking built agent artifacts

--- ayin/ayin-agent/dist/config.d.ts
+++
@@ -1,4 +0,0 @@
-import { AgentConfig } from './types';
-export declare function loadConfig(): AgentConfig;
-export declare function validateConfig(config: AgentConfig): void;
-//# sourceMappingURL=config.d.ts.map
